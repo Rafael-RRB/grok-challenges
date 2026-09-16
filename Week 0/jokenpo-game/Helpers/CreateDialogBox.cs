82d579871924fe978241c14823a4e7348fc913f5
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2026-09-15 23:44:07 -0300

    feat: Added a way to create ASCII dialog boxes using string arrays.

---
+++ Week 0/jokenpo-game/Helpers/CreateDialogBox.cs
@@ -0,0 +1,35 @@
+﻿using JokenpoTerminal.Utilities;
+
+namespace JokenpoTerminal.Helpers
+{
+    public static class DialogBox
+    {
+        public static string[] Create(string[] messages)
+        {
+            if (messages == null || messages.Length == 0)
+            {
+                return new string[] { "####", "#  #", "####" };
+            }
+
+            int contentWidth = messages.MaxLength();
+            int totalWidth = contentWidth + 4;
+
+            string outerBorder = new string('#', totalWidth);
+            string innerBorder = $"# { new string(' ', contentWidth)} #";
+
+            string[] dialogBox = new string[messages.Length + 4];
+            dialogBox[0] = outerBorder;
+            dialogBox[1] = innerBorder;
+            dialogBox[dialogBox.Length - 2] = innerBorder;
+            dialogBox[dialogBox.Length - 1] = outerBorder;
+
+
+            for (int i = 0; i < messages.Length; i++)
+            {
+                dialogBox[2 + i] = $"# { messages[i]} #";
+            }
+
+            return dialogBox;
+        }
+    }
+}
