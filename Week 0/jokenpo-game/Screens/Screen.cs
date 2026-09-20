85391516afd0a7f71ac5397c952b1ef7355bc199
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2026-09-20 17:07:28 -0300

    refactor: Removed unused fields.

--- Week 0/jokenpo-game/Screens/Screen.cs
+++ Week 0/jokenpo-game/Screens/Screen.cs
@@ -5,9 +5,6 @@ namespace JokenpoTerminal.Screens
 {
     public abstract class Screen
     {
-        private readonly Renderer renderer = null!;
-        private readonly Assets assets = null!;
-
         public virtual void Enter(Renderer renderer, Assets assets) { }
 
         public virtual void Update(Renderer renderer, Assets assets, double deltaTime) { }
