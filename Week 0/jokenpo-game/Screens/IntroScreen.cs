f4c6ff524d2e7f5b97ae102b77517f468ffe964f
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2026-09-20 17:13:04 -0300

    feat: Intro animation finished.

--- Week 0/jokenpo-game/Screens/IntroScreen.cs
+++ Week 0/jokenpo-game/Screens/IntroScreen.cs
@@ -2,6 +2,7 @@
 using JokenpoTerminal.Render;
 using JokenpoTerminal.Structs;
 using JokenpoTerminal.Game;
+using JokenpoTerminal.Helpers;
 
 namespace JokenpoTerminal.Screens
 {
@@ -12,15 +13,19 @@ namespace JokenpoTerminal.Screens
         private Layer triangleLayer = null!;
         private Layer squareLayer = null!;
         private Layer circleLayer = null!;
+        private Layer messageLayer = null!;
         private int leftOffset = 0;
         private double sparkleTimer = 0;
         private double triangleTimer = 0;
         private double squareTimer = 0;
         private double circleTimer = 0;
         private double slideTimer = 0;
+        private double introTimer = 0;
         private int triangleDirection = -1;
         private int squareDirection = -1;
         private int circleDirection = 1;
+        private bool introFinished = false;
+
         public override void Enter(Renderer renderer, Assets assets)
         {
             leftOffset = renderer.LogicalWidth;
@@ -65,23 +70,33 @@ namespace JokenpoTerminal.Screens
                 HorizontalAlign = XAlign.Center,
                 VerticalAlign = YAlign.Middle,
             };
+            messageLayer = new Layer
+            {
+                Content = DialogBox.Create("Press any key to start..."),
+                Position = new Position(0, 0),
+                Color = ConsoleColor.White,
+                HorizontalAlign = XAlign.Center,
+                VerticalAlign = YAlign.Bottom,
+                IsVisible = false
+            };
         }
         public override void Update(Renderer renderer, Assets assets, double deltaTime)
         {
-            // Is there a less repeat-y way of doing this?
+            #region Add time to all basic timers
             sparkleTimer += deltaTime;
             triangleTimer += deltaTime;
             squareTimer += deltaTime;
             circleTimer += deltaTime;
             slideTimer += deltaTime;
+            #endregion
 
+            #region Animate all basic layers
             if (sparkleTimer >= 1.0)
             {
                 sparkleTimer -= 1.0;
                 sparkleLayer.IsVisible = sparkleLayer.IsVisible ? false : true;
             }
 
-            // Triangle Animation
             if (triangleTimer >= 0.5)
             {
                 triangleTimer -= 0.5;
@@ -96,7 +111,6 @@ namespace JokenpoTerminal.Screens
                 }
             }
 
-            // Square Animation
             if (squareTimer >= 0.5)
             {
                 squareTimer -= 0.5;
@@ -111,7 +125,6 @@ namespace JokenpoTerminal.Screens
                 }
             }
 
-            // Circle Animation
             if (circleTimer >= 0.5)
             {
                 circleTimer -= 0.5;
@@ -125,20 +138,52 @@ namespace JokenpoTerminal.Screens
                     circleDirection = 1;
                 }
             }
+            #endregion
 
+            #region Slide frames into place
             if (leftOffset > 0)
             {
                 if (slideTimer >= 1.0 / 20.0)
                 {
                     slideTimer -= 1.0 / 20.0;
-                    leftOffset = leftOffset - 1;
+                    leftOffset = Math.Max(0, leftOffset - 1); // Prevents negative values and weird rounding errors
                 }
             }
+            #endregion
 
-            foreach (Layer layer in new Layer[5] { protagonistLayer, sparkleLayer, triangleLayer, squareLayer, circleLayer })
+            #region Menu logic
+            else if (!introFinished && leftOffset == 0)
             {
-                layer.Position.X = leftOffset;
+                introFinished = true;
             }
+
+            if (introFinished)
+            {
+                introTimer += deltaTime;
+            }
+
+            if (introTimer >= 1)
+            {
+                messageLayer.IsVisible = !messageLayer.IsVisible;
+
+                if (messageLayer.IsVisible)
+                {
+                    introTimer -= 1;
+                }
+                else
+                {
+                    introTimer -= 0.5;
+                }
+            }
+            #endregion
+
+            #region Apply offset to all basic layers
+            protagonistLayer.Position.X = leftOffset;
+            sparkleLayer.Position.X = leftOffset;
+            triangleLayer.Position.X = leftOffset;
+            squareLayer.Position.X = leftOffset;
+            circleLayer.Position.X = leftOffset;
+            #endregion
         }
 
         public override void Draw(Renderer renderer, Assets assets)
@@ -148,6 +193,7 @@ namespace JokenpoTerminal.Screens
             renderer.AddLayer(squareLayer);
             renderer.AddLayer(circleLayer);
             renderer.AddLayer(protagonistLayer);
+            renderer.AddLayer(messageLayer);
         }
         public override void HandleInput(ConsoleKeyInfo key)
         {
@@ -156,10 +202,16 @@ namespace JokenpoTerminal.Screens
 
         public override void Exit(Renderer renderer, Assets assets)
         {
-            foreach (Layer layer in new Layer[5] { protagonistLayer, sparkleLayer, triangleLayer, squareLayer, circleLayer})
-            {
-                layer.Position = Position.Zero;
-            }   
+            sparkleTimer = 0;
+            triangleTimer = 0;
+            squareTimer = 0;
+            circleTimer = 0;
+            slideTimer = 0;
+            introTimer = 0;
+            triangleDirection = -1;
+            squareDirection = -1;
+            circleDirection = 1;
+            introFinished = false;
         }
 
         public IntroScreen() {}
