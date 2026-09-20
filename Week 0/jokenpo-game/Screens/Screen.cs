f273cca46581e18d5ad438a41ef61432706a6c44
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2026-09-20 20:55:16 -0300

    feat: Screens now have an AllowInput property.

--- Week 0/jokenpo-game/Screens/Screen.cs
+++ Week 0/jokenpo-game/Screens/Screen.cs
@@ -5,6 +5,8 @@ namespace JokenpoTerminal.Screens
 {
     public abstract class Screen
     {
+        public bool AllowInput { get; protected set; } = true; 
+
         public virtual void Enter(Renderer renderer, Assets assets) { }
 
         public virtual void Update(Renderer renderer, Assets assets, double deltaTime) { }
